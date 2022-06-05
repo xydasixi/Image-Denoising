8e5e0b33b10112ef6ee518c3570757f9d714b8f4
xydasixi <1766187138xy@gmail.com>
2022-06-05 21:06:03 +0800

    4

--- test.py
+++ test.py
@@ -57,13 +57,26 @@ if __name__ == '__main__':
     model = torch.load(file_list[-1])
     model.eval()
     model = model.to(DEVICE)
+    psnrs = []
+    ssims = []
+
     for im in os.listdir(DATA_PATH):
         if im.endswith(".jpg") or im.endswith(".bmp") or im.endswith(".png"):
             x = np.array(imread(os.path.join(DATA_PATH, im)), dtype=np.float32)/255.0
             y = x + np.random.normal(0, SIGMA / 255.0, x.shape)  # Add Gaussian noise without clipping
             y = y.astype(np.float32)
             y_ = torch.from_numpy(y).view(1, -1, y.shape[0], y.shape[1])
-
-            torch.cuda.synchronize()
             start_time = time.time()
             y_ = y_.to(DEVICE)
+            x_ = model(y_)
+            x_ = x_.view(y.shape[0], y.shape[1])
+            psnr_temp = peak_signal_noise_ratio(x, x_)
+            ssim_temp = structural_similarity(x, x_)
+            psnrs.append(psnr_temp)
+            ssims.append(ssim_temp)
+    psnr_avg = np.mean(psnrs)
+    ssim_avg = np.mean(ssims)
+    psnrs.append(psnr_avg)
+    ssims.append(ssim_avg)
+    np.savetxt('results/test_result.txt' , np.hstack((psnrs, ssims)),fmt='%2.4f')
+
